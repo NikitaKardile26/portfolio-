6194062fe17fb2dba24ee5171f536cab6648f00a
Nikita kardile <132800661+NikitaKardile26@users.noreply.github.com>
2024-01-25 22:09:26 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,209 @@
+export const Bio = {
+  name: "Kardile Nikita",
+  roles: [
+    "Web Developer",
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/NikitaKardile26",
+  resume:
+    "https://drive.google.com/file/d/18tfQvajRiNWfTdooCBN8ayTylOsZUF9g/view?usp=drivesdk",
+  linkedin: "https://www.linkedin.com/in/KardileNikita",
+  twitter: "#",
+  insta: "#",
+  facebook: "#",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "PHP",
+        image:
+          "https://www.php.net//images/logos/new-php-logo.svg",
+      },
+            {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "XML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+
+export const education = [
+  {
+    id: 0,
+    img: "https://images.collegedunia.com/public/college_data/images/logos/1705988727JSPMlogo.jpeg?h=80&w=80&mode=stretch",
+    school: "JSPM Rajarshi Shahu College Of Engineering , Tathawade",
+    date: "  Nov 2022 - May 2024",
+    grade: "8.7 CGPA",
+    desc: "I am currently pursuing a Master's degree in Computer application at JSPM Rajarshi Shahu College Of Engineering , Tathawade. I have completed 3 semesters and have a CGPA of 8.7. In the academic curriculum i have learn various subjects such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. where I am learning, improving skills and working on exciting projects.",
+    degree: "Master of Computer Application",
+  },
+  {
+    id: 1,
+    img: "https://www.ctboracollege.edu.in/images/College-logo1.jpg",
+    school: "C. T. Bora Arts, Science & Commerce college, Shirur, Pune",
+    date: "Apr 2019 - Apr 2022",
+    grade: "9.1 CGPA",
+    desc: "I completed my Bachelor's degree in Computer Science at C. T. Bora Arts, Science & Commerce college, Shirur. Where i learn programming languages and computer fundamental concepts and also various skills such as C, java, php, frontend technologies and so on",
+    degree: "Bachelor of Computer Science",
+  },
+  {
+    id: 2,
+    img: "https://www.ctboracollege.edu.in/images/College-logo1.jpg",
+    school: "C. T. Bora Arts, Science & Commerce college, Shirur, Pune",
+    date: "Apr 2018 - Apr 2019",
+    grade: "60.30%",
+    desc: "I completed my class 12 high school education at C. T. Bora Arts, Science & Commerce college, Shirur. where I studied Science with PCMB Group.",
+    degree: "HSC(XII), Science with PCMB Group",
+  },
+  {
+    id: 3,
+    img: "https://www.ctboracollege.edu.in/images/College-logo1.jpg",
+    school: "Vidyadham Prashala Shirur,Pune.",
+    date: "Apr 2016 - Apr 2017",
+    grade: "87.80%",
+    desc: "I completed my class 10 education at Vidyadham Prashala Shirur,Pune. where I completed my secondary education",
+    degree: "SSC(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "food-ordering-system",
+    date: "Nov 2022 - Mar 2023",
+    description:
+      " The food ordering system is MERN Stack Ecommerce Project. The proposed system is developed to manage ordering activities in fast food restaurant. These systems will ultimately allow you to better manage resources ",
+    image:
+      "https://github.com/HusenMujawar/food-ordering-system/blob/main/Screenshot%202024-01-08%20141200.png?raw=true",
+    tags: ["HTML5 & CSS3",  "Bootstrap","Javascript", "MERN Technology"],
+    category: "web app",
+    github: "https://github.com/NikitaKardile26/Food-Ordering-System-.git",
+  },
+  {
+    id: 1,
+    title: "Book_Shelf",
+    date: "May 2023 - Jul 2023",
+    description:
+      "Bookshelf is an e-commerce website for online shopping of books. This system improves the method of taking the order from customer. In this system user can browse books according to their price,ratings and geners .",
+    image:
+      "https://github.com/NikitaKardile26/Bookshelf/blob/main/Screenshot.png",
+    tags: ["Html", "CSS", "Javascript", "PHP", "MYSQL"],
+    category: "web app",
+    github: "https://github.com/NikitaKardile26/Bookshelf.git",
+  },
+  {
+    id: 2,
+    title: "Portfolio website",
+    date: "Jan 2024",
+    description:
+      "A portfolio react based responsive website. This project showcases my skills, projects, and experiences as a web developer.",
+    image:
+      "https://github.com/HusenMujawar/Portfolio/blob/main/src/images/portfolioimage.png?raw=true",
+    tags: ["HTML5", "CSS", "Javascript", "ReactJS"],
+    category: "web app",
+    github: "https://github.com/HusenMujawar/Portfolio.git",
+  },
+ 
+];
+
